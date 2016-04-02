509c86f0534ab1d64cdfed262b895a40b4dd18e0
jeremy <spirography@users.noreply.github.com>
2016-04-02 09:58:02 -0400

    Simple page for displaying errors or stuff

---
+++ templates/apology.php
@@ -0,0 +1,16 @@
+<br>
+<br>
+<br>
+<br>
+<div style="margin: auto; padding: 20px; width: 300px; border: 1px solid #777777; border-radius: 8px;">
+
+<p class="lead text-danger">
+    Sorry!
+</p>
+<p class="text-danger">
+    <?= htmlspecialchars($message) ?>
+</p>
+
+<a href="javascript:history.go(-1);">Back</a>
+
+</div>
