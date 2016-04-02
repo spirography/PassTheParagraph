50bae872d8f752277c311ca60dd1db08b826667c
jeremy <spirography@users.noreply.github.com>
2016-04-01 23:11:57 -0400

    renamed to right name

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    /**
+     * config.php
+     *
+     * Configures pages and other stuff
+     */
+
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
+    // requirements
+    require_once("constants.php");
+    require("functions.php");
+
+?>
---
+++ includes/constants.php
@@ -0,0 +1,23 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     *
+     * Global constants for everything
+     * Mostly database stuff like passwords
+     */
+
+    // your database's name
+    define("DATABASE", "pass_the_paragraph");
+
+    // your database's password
+    define("PASSWORD", "123456789");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "root");
+
+?>
