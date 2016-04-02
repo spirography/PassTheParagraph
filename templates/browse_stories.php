b460cc509c3ee33672ffd8057dcf6f8f4d8186a6
jeremy <spirography@users.noreply.github.com>
2016-04-02 08:50:38 -0400

    More tentative styling for story previews

--- templates/browse_stories.php
+++ templates/browse_stories.php
@@ -3,4 +3,12 @@
 </div>
 <div id="previews">
 
+    <!-- EXAMPLE STORY -->
+    <div class="story-preview">
+        <h1>The Call to Cthulu</h1>
+        <p>It was a dark and stormy night.</p>
+        <span>10 contributions</span><span>last updated 8:35</span>
+    </div>
+    <!-- EXAMPLE STORY END -->
+
 </div>
