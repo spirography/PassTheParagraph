fcc6b707be6967b476eea263e83c10f07ac68141
jeremy <spirography@users.noreply.github.com>
2016-04-01 22:56:52 -0400

    Added header and footer templates

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- CSS References Go Here -->
+
+    <!-- Favicon References Go Here -->
+
+    <!-- Title -->
+    <?php if (isset($title)): ?>
+    <title>
+      <?= htmlspecialchars($title) ?>
+    </title>
+    <?php else: ?>
+    <title>HH Legendary
+    </title>
+    <?php endif ?>
+
+    <!-- JavaScript References -->
+  </head>
+
+  <!-- Body Start -->
+  <body>
+      <div id="container">
+          <!-- Navbar Start -->
+
+          <!-- Navbar End -->
+
+          <!-- Content Goes Here -->
