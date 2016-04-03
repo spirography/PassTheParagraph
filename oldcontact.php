d2cb292ce3d60726e2d8122cca0c12816d9a4d5e
Scott Giles <scottgiles@dhcp-10-5-23-17.wireless.rochester.edu>
2016-04-03 01:00:55 -0400

    Temp update with browse/contact

---
+++ oldcontact.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pass the paragraph.</title>
+
+
+	<!-- default -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- bootstrap and jQuery-->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+ 	<!-- Font awesome for icons-->
+	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<!-- Font for nav bar -->
+	<link href='https://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css">
+	<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+	<script src="js/script.js"></script>
+
+
+</head>
+
+
+
+
+<body>
+
+<!-- Navigation bar -->
+<nav class="navbar navbar-default navbar-top">
+
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span></button>
+			<!-- <img src="img/logo.png" href="pass the paragraph logo" width="35"> -->
+			<a id="logo" class="navbar-brand" href="index.php">pass the paragraph<span class="period">.</span></a>
+
+		</div>
+
+
+
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php">home</a></li>
+				<li class="dropdown"><a class ="dropdown-toggle" data-toggle="dropdown" href="browse.php">browse<span class="caret"></span></a>
+	 				<ul class="dropdown-menu">
+		 				<li><a href="browse.php?g=1">Realistic Fiction</a></li>
+		 				<li><a href="browse.php?g=2">Science Fiction</a></li>
+		 				<li><a href="browse.php?g=3">Mystery</a></li>
+		 				<li><a href="browse.php?g=4">Fantasy</a></li>
+						<li><a href="browse.php?g=5">Historical</a></li>
+						<li><a href="browse.php?g=6">Comedy</a></li>
+	 				</ul>
+				</li>
+				<li><a href="submit-story.php">submit</a></li>
+				<li><a href="about.php">contact</a></li>
+			</ul>
+		</div>
+
+	</div>
+
+  <div class="container-fluid">
+		<div class="about-body">
+      <center><font size="10">Contact Info</font></center>
+      <center><font size="6">Mailing Address<span class="period">:</span></font></center>
+      <center>500 Joseph C<span class="period">.</span> Wilson Blvd<span class="period">.</span></center>
+      <center>Rochester<span class="period">,</span> NY 14627</center>
+      <center><font size="6">E<span class="period">-</span>mail<span class="period">:</span></font></center>
+      <center>wedonthaveanemailyet@example<span class="period">.</span>com</center>
+      <center><font size="5">pass the paragraph<span class="period">.</span></font></center>
+
+		</div>
+</div>
+
+
+</nav>
